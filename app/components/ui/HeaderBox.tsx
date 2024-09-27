7163b7781b9cd50ef18fe616cfa2c50b9d117244
programadoragain <ferdandi04@gmail.com>
2024-09-26 21:25:54 -0300

    HeaderBox component

--- app/components/ui/HeaderBox.tsx
+++ app/components/ui/HeaderBox.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function HeaderBox({ type, title, user, description }: HeaderBoxProps) {
   return (
-    <div>HeaderBox</div>
+    <div className="header-box">
+        <h1 className="header-box-title">
+            {title}
+            {type === 'greeting' && (
+            <span className="text-bankGradient">
+                {`Welcome ${user}, let's start holding your crypto!`}
+            </span>
+        </h1>    
+        )}
+    </div>
   )
 }
 
